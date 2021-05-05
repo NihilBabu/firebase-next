5f2978bd4c8f46008cae187afbfd93cc0524fc1d
NihilBabu <babunihil@gmail.com>
2021-05-05 19:01:33 +0530

    init

---
+++ firebase/firebaseAdmin.js
@@ -0,0 +1,19 @@
+import admin from "firebase-admin";
+
+const serviceAccount = require("../secret.json")
+
+export const verifyIdToken = (token) => {
+    if (!admin.apps.length) {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: "https://fir-auth-5e36f.firebaseapp.com"
+        })
+    }
+
+    return admin
+        .auth()
+        .verifyIdToken(token)
+        .catch((err) => {
+            throw err
+        })
+}
---
+++ firebase/firebaseClient.js
@@ -0,0 +1,19 @@
+import { firebase } from "firebase";
+
+const FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+
+export default function firebaseClient() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(FIREBASE_CONFIG);
+  }
+
+}
